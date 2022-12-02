133ad88c71cdf015bb87d965726cb194cc41b417
Ian Griffiths <Ian.Griffiths@confused.com>
2022-12-02 15:41:15 +0000

    Benchmarked

---
+++ Challenge2/Benchmarker.cs
@@ -0,0 +1,31 @@
+﻿namespace Challenge2
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Order;
+
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class Benchmarker
+    {
+        //private Round[] _rounds;
+
+        //[GlobalSetup]
+        //public async Task Setup()
+        //{
+        //    var input = await File.ReadAllLinesAsync("./input.txt");
+        //    _rounds = input.Select(i => new Round(i)).ToArray();
+        //}
+
+        [Benchmark]
+        public async Task RunAnalysis()
+        {
+            var input = await File.ReadAllLinesAsync("./input.txt");
+            var rounds = input.Select(i => new Round(i)).ToArray();
+            var scoreBeforeFullRequirements = rounds.Sum(i => i.RoundScoreBefore);
+            var scoreAfterFullRequirements = rounds.Sum(i => i.RoundScoreAfter);
+
+            Console.WriteLine($"Score before full requirements: {scoreBeforeFullRequirements}; score after full requirements: {scoreAfterFullRequirements}.");
+        }
+    }
+}
--- Challenge2/Program.cs
+++ Challenge2/Program.cs
@@ -1,8 +1,4 @@
-﻿using Challenge2;
+﻿using BenchmarkDotNet.Running;
+using Challenge2;
 
-var input = await File.ReadAllLinesAsync("./input.txt");
-var rounds = input.Select(i => new Round(i)).ToArray();
-var scoreBeforeFullRequirements = rounds.Sum(i => i.RoundScoreBefore);
-var scoreAfterFullRequirements = rounds.Sum(i => i.RoundScoreAfter);
-
-Console.WriteLine($"Score before full requirements: {scoreBeforeFullRequirements}; score after full requirements: {scoreAfterFullRequirements}");
+BenchmarkRunner.Run<Benchmarker>();
