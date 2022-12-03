a856452192cd118fb813c1014327162a9dd0c98f
Ian Griffiths <Ian.Griffiths@confused.com>
2022-12-03 07:42:43 +0000

    Tidied up Program

--- Challenge3/Program.cs
+++ Challenge3/Program.cs
@@ -2,5 +2,5 @@
 
 var input = await File.ReadAllLinesAsync("./input.txt");
 var rucksacks = input.Select(i => new Rucksack(i)).ToArray();
-var groups = rucksacks.Chunk(3).Select(group => new Group(group)).ToList();
+var groups = rucksacks.Chunk(3).Select(group => new Group(group));
 Console.WriteLine($"Sum of priorities: {rucksacks.Sum(r => r.CommonItemPriority)}; Sum of Badge priorities: {groups.Sum(g => g.BadgePriority)}");
