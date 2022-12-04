c39ab3889846a00aed92454986beb748d7b31715
Ian Griffiths <Ian.Griffiths@confused.com>
2022-12-04 08:29:06 +0000

    Easy way of doing it

---
+++ Challenge4/Assignment.cs
@@ -0,0 +1,20 @@
+﻿namespace Challenge4
+{
+    internal class Assignment
+    {
+        private IEnumerable<int> FirstElfSections { get; }
+        private IEnumerable<int> SecondElfSections { get;}
+
+        public bool IsOverlap => FirstElfSections.Any(f => SecondElfSections.Contains(f));
+        public bool OneContainedInOther => SecondElfSections.All(s => FirstElfSections.Contains(s)) 
+                                        || FirstElfSections.All(f => SecondElfSections.Contains(f));
+
+        internal Assignment(string input)
+        {
+            var ranges = input.Split('-',',');
+
+            FirstElfSections = Enumerable.Range(int.Parse(ranges[0]), int.Parse(ranges[1]) - int.Parse(ranges[0]) + 1);
+            SecondElfSections = Enumerable.Range(int.Parse(ranges[2]), int.Parse(ranges[3]) - int.Parse(ranges[2]) + 1);
+        }
+    }
+}
---
+++ Challenge4/Program.cs
@@ -0,0 +1,6 @@
+﻿using Challenge4;
+
+var input = await File.ReadAllLinesAsync("./input.txt");
+var assignments = input.Select(i => new Assignment(i)).ToArray();
+
+Console.WriteLine($"Number of fully contained assignments = {assignments.Count(a => a.OneContainedInOther)}; number overlapping = {assignments.Count(a => a.IsOverlap)}");
