dfcac8efccfe4f7d490a799eccacc8b77bf9c510
Ian Griffiths <Ian.Griffiths@confused.com>
2022-12-05 06:44:51 +0000

    Refactored crate moving into cratestack class

--- Challenge5/CrateStack.cs
+++ Challenge5/CrateStack.cs
@@ -4,9 +4,29 @@
     {
         internal Stack<Crate> Crates { get; }
 
-        internal CrateStack(Stack<Crate> crates)
+        internal void AddCrates(Crate[] cratesToAdd)
         {
-            Crates = crates;
+            // foreach (var crate in cratesToAdd.Reverse()) // Easiest answer to part 2
+            foreach (var crate in cratesToAdd) 
+            {
+                 Crates.Push(crate);
+            }
+        }
+
+        internal Crate[] RemoveCrates(int numberToRemove)
+        {
+            var cratesRemoved = new List<Crate>();
+            for (var i = 0; i < numberToRemove; i++)
+            {
+                cratesRemoved.Add(Crates.Pop());
+            }
+
+            return cratesRemoved.ToArray();
+        }
+
+        internal CrateStack(Crate[] crates)
+        {
+            Crates = new Stack<Crate>(crates.Reverse());
         }
     }
 }
--- Challenge5/Program.cs
+++ Challenge5/Program.cs
@@ -8,25 +8,8 @@ foreach (var line in input)
     if (line.StartsWith("move"))
     {
         var move = new Move(line);
-        // Mechanism for Part 1
-        //for (var i = 1; i <= move.NumberToMove; i++)
-        //{
-        //    var crate = crateStacks[move.SourceColumn].Crates.Pop();
-        //    crateStacks[move.DestinationColumn].Crates.Push(crate);
-        //}
-
-        // Mechanism for Part 2
-        var cratesToMove = new List<Crate>();
-        for (var i = 1; i <= move.NumberToMove; i++)
-        {
-            var crate = crateStacks[move.SourceColumn].Crates.Pop();
-            cratesToMove.Add(crate);
-        }
-        cratesToMove.Reverse();
-        foreach (var crate in cratesToMove)
-        {
-            crateStacks[move.DestinationColumn].Crates.Push(crate);
-        }
+        var removedCrates= crateStacks[move.SourceColumn].RemoveCrates(move.NumberToMove);
+        crateStacks[move.DestinationColumn].AddCrates(removedCrates);
     }
     else if (line.StartsWith('['))
     {
@@ -39,7 +22,6 @@ foreach (var line in input)
     }
     else if (string.IsNullOrEmpty(line))
     {
-        continue;
     }
     else
     {
@@ -47,10 +29,8 @@ foreach (var line in input)
 
         foreach (var stackId in stackIds.Where(sid => !string.IsNullOrEmpty(sid)))
         {
-            var stackOfCrates = new Stack<Crate>(tempCratesInStack.Where(cis => cis.Item1 == int.Parse(stackId))
-                .Select(cis => cis.Item2).ToArray().Reverse());
-
-            crateStacks.Add(int.Parse(stackId), new CrateStack(stackOfCrates));
+            crateStacks.Add(int.Parse(stackId), new CrateStack(tempCratesInStack.Where(cis => cis.Item1 == int.Parse(stackId))
+                .Select(cis => cis.Item2).ToArray()));
         }
     }
 }
