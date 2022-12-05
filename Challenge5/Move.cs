53e7b7f481e045c0ec857c5a5f71563c79e84747
Ian Griffiths <Ian.Griffiths@confused.com>
2022-12-05 10:11:39 +0000

    Property renames

--- Challenge5/Move.cs
+++ Challenge5/Move.cs
@@ -3,20 +3,20 @@
 internal class Move
 {
     private int NumberToMove { get; }
-    private int SourceStack { get; }
-    private int DestinationStack { get; }
+    private int SourceStackIndex { get; }
+    private int DestinationStackIndex { get; }
 
     internal void ApplyMoveOperations(List<CrateStack> stacks)
     {
-        var removedCrates = stacks[SourceStack].RemoveCrates(NumberToMove);
-        stacks[DestinationStack].AddCrates(removedCrates);
+        var removedCrates = stacks[SourceStackIndex].RemoveCrates(NumberToMove);
+        stacks[DestinationStackIndex].AddCrates(removedCrates);
     }
 
     internal Move(string input)
     {
         var elements = input.Split(' ');
         NumberToMove = Convert.ToInt32(elements[1]);
-        SourceStack = Convert.ToInt32(elements[3]) - 1; // 0 based index
-        DestinationStack = Convert.ToInt32(elements[5]) - 1; // 0 based index
+        SourceStackIndex = Convert.ToInt32(elements[3]) - 1; // 0 based index
+        DestinationStackIndex = Convert.ToInt32(elements[5]) - 1; // 0 based index
     }
 }
