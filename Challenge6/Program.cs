3d4648fd42127468613e78e0ccc8efb63e0c1f73
Ian Griffiths <Ian.Griffiths@confused.com>
2022-12-06 06:56:10 +0000

    Reduced the line count again

--- Challenge6/Program.cs
+++ Challenge6/Program.cs
@@ -1,6 +1,5 @@
 ﻿var input = await File.ReadAllTextAsync("./Input.txt");
 const int requiredSequenceLength = 4; // 14 for second part
 var count = requiredSequenceLength;
-while (input[(count - requiredSequenceLength)..count++].Distinct().Count() != requiredSequenceLength)
-{ }
+while (input[(count - requiredSequenceLength)..count++].Distinct().Count() != requiredSequenceLength){ }
 Console.WriteLine($"End position of first unique sequence of {requiredSequenceLength} = {count-1}");
