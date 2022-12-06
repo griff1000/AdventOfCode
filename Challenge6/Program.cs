7fb29ba717a9e6e04a627cc414c5f9be81119e9d
Ian Griffiths <Ian.Griffiths@confused.com>
2022-12-06 06:36:24 +0000

    Challenge 6

---
+++ Challenge6/Program.cs
@@ -0,0 +1,8 @@
+﻿var input = await File.ReadAllTextAsync("./Input.txt");
+const int requiredSequenceLength = 14; // 4 for first part
+var count = requiredSequenceLength;
+while (input[(count - requiredSequenceLength)..count].Distinct().Count() != requiredSequenceLength)
+{
+    count++;
+}
+Console.WriteLine($"End position of first unique sequence of {requiredSequenceLength} = {count}");
