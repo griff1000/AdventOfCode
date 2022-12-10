07c42a239da55049f4d70804314b1b056c9da7c6
Ian Griffiths <Ian.Griffiths@confused.com>
2022-12-10 11:34:25 +0000

    Tidied up

---
+++ Challenge10/CRT.cs
@@ -0,0 +1,43 @@
+﻿namespace Challenge10;
+
+using System.Text;
+using Microsoft.VisualBasic;
+
+internal class CRT
+{
+    internal List<List<bool>> Pixels { get; } = new ();
+
+    internal CRT()
+    {
+        for (var r = 0; r <= 6; r++)
+        {
+            var row = new List<bool>();
+            for (var c = 0; c < 40; c++)
+            {
+                row.Add(false);
+            }
+            Pixels.Add(row);
+        }
+    }
+
+    internal void ProcessPixel(int cycle, int registerValue)
+    {
+        var horizontal = (cycle - 1) % 40;
+        var vertical = (cycle - 1) / 40;
+        var isLit = (horizontal - 1) <= registerValue && (horizontal + 1) >= registerValue;
+        Pixels[vertical][horizontal] = isLit;
+    }
+
+    internal void RenderCRT()
+    {
+        foreach (var row in Pixels)
+        {
+            var sb = new StringBuilder();
+            foreach (var pixel in row)
+            {
+                sb.Append(pixel ? '*' : ' ');
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
--- Challenge10/Program.cs
+++ Challenge10/Program.cs
@@ -1,36 +1,16 @@
-﻿using System.Text;
-using Challenge10;
-using Microsoft.VisualBasic;
+﻿using Challenge10;
 
 var input = await File.ReadAllLinesAsync("./Input.txt");
 var cpu = new CPU();
 var instructions = input.Select(i => new Instruction(i)).ToList();
-var cycle = 0;
 var signalStrengths = new Dictionary<int, int>();
-var crt = new List<List<bool>>();
-for (var r = 0; r <= 6; r++ )
-{
-    var row = new List<bool>();
-    for (var c = 0; c < 40; c++)
-    {
-        row.Add(false);
-    }
-    crt.Add(row);
-}
+var crt = new CRT();
 
-var horizontal = -1;
-var vertical = 0;
+var cycle = 0;
 for (var i = 0; i < instructions.Count; i++)
 {
     cycle++;
-    horizontal++;
-    if (horizontal > 39)
-    {
-        horizontal = 0;
-        vertical++;
-    }
-    var isLit = (horizontal - 2) < cpu.Register && (horizontal + 2) > cpu.Register;
-    crt[vertical][horizontal] = isLit;
+    crt.ProcessPixel(cycle, cpu.Register);
 
     if (cycle == 20 || (cycle + 20) % 40 == 0)
     {
@@ -40,12 +20,4 @@ for (var i = 0; i < instructions.Count; i++)
     if (!instructions[i].InstructionComplete) i--; // do same instruction until complete
 }
 Console.WriteLine($"Sum of first six signal strengths: {signalStrengths.Take(6).Sum(ss => ss.Value)}");
-foreach (var row in crt)
-{
-    var sb = new StringBuilder();
-    foreach (var pixel in row)
-    {
-        sb.Append(pixel ? '#' : '.');
-    }
-    Console.WriteLine(sb.ToString());
-}
+crt.RenderCRT();
